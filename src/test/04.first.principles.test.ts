7bb3809201b3c2a63e65117e5b075234cf51fbde
Thomaz Alves <thomazhilario5@gmail.com>
2024-08-20 16:29:03 -0300

    Entendendo princípio F.I.R.S.T

---
+++ src/test/04.first.principles.test.ts
@@ -0,0 +1,10 @@
+/*
+    -> F.I.R.S.T Princíples
+    - Princípios que podemos seguir ao escrever os testes.
+
+    - Fast: Os testes devem ser rápidos para se obter feedbacks mais rápidos.
+    - Independent: Os testes sem estados compartilhados, não devem ter um estado compartilhado com outros testes.
+    - Repeateble: A mesma entrada deve ter o mesmo resultado.
+    - Self validating: Os resultados dos testes após conclusão devem ser claros. (passou ou falhou)
+    - Thorough: Testar casos de sucessos e casos de erros, valores grandes...
+*/
